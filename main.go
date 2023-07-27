d0c9627c0ea7904f854aa24b5a80f1fc38b269cd
adriano-henrique <adrianohenriqueafonso@gmail.com>
2023-07-27 12:33:25 -0300

    Adding extra handlers

--- main.go
+++ main.go
@@ -1,34 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"time"
 )
 
-func middlewareCors(next http.Handler) http.Handler {
+type metricsApiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *metricsApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+		cfg.fileServerHits++
 		next.ServeHTTP(w, r)
 	})
 }
 
-func main() {
-	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+func (cfg *metricsApiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+}
 
-	corsMux := middlewareCors(serverMux)
-	newServer := &http.Server{
-		Addr:           ":8080",
-		Handler:        corsMux,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   20 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+func main() {
+	const port = "8080"
+	const filePathRoot = "."
+
+	configMetrics := metricsApiConfig{
+		fileServerHits: 0,
 	}
-	newServer.ListenAndServe()
+
+	r := chi.NewRouter()
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", configMetrics.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	serveMux.HandleFunc("/healthz", handlerReadiness)
+	serveMux.HandleFunc("/metrics", configMetrics.handlerMetrics)
+
+	corsMux := middlewareCors(serveMux)
+	newServer := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(newServer.ListenAndServe())
 }
